4312f3bc7815a29ef589cc96c540018567001c73
Yuto Ichihara <yutounwasese@gmail.com>
2022-04-17 08:53:31 +0900

    Improve UI  of results page

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,20 +1,87 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { loading, focusInput, data, onSearch, isIncludingTitle, isNotIncludingTitle, isIncludingBody, isIncludingCreated, isIncludingUpdated, searchKeyword } from './OptionStates';
+import { loading, data } from './OptionStates';
 </script>
 
 <template>
-  <v-row>
-    <ul>
-      <li v-for="li in data" :key="li.id">
-        <a :href="li.url">{{ li.title }}</a>
-      </li>
-    </ul>
-  </v-row>
-  <v-row>
-    {{ loading }}
-  </v-row>
+  <v-card
+    class="mx-auto"
+  >
+    <v-toolbar
+      color="success"
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Qiita Search</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-list three-line
+      class="mx-auto"
+      max-width="1000"
+    >
+      <template 
+        v-for="item in data" 
+        :key="item.title" 
+      >
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img :src="item.user.profile_image_url" class="img rounded-circle d-inline-block"></v-img>
+          </v-list-item-avatar>
+          <v-list-item-content class="ml-10">
+            <v-list-item-title>
+              <a :href="item.url" class="title">
+                {{ item.title }}
+              </a>
+            </v-list-item-title>
+            <div class="tags">
+              <div 
+                v-for="tag in item.tags" 
+                :key="tag.name"
+                
+              >
+                <v-list-item-subtitle >
+                  <v-btn
+                    rounded
+                    x-small
+                    color="success"
+                    dark
+                    class="button"
+                  >
+                    {{ tag.name }}
+                  </v-btn>
+                </v-list-item-subtitle>
+              </div>
+            </div>
+            
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-card>
 </template>
 
 <style scoped>
+.img {
+  width: 5vh;
+  height: 5vhpx;
+}
+.button {
+  font-size: 10px;
+  height: 15px;
+  padding: 1vh;
+  
+}
+.title {
+  color:green;
+}
+.tags {
+  display: flex;
+}
 </style>
